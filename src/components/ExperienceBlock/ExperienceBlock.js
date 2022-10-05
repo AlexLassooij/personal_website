3770ebc40f6f47dc24d0c7487f774936cd2ef4f0
AlexLassooij <alex.lassooij@icloud.com>
2022-10-05 14:43:11 -0700

    Added Kardium and ELEC 391

--- src/components/ExperienceBlock/ExperienceBlock.js
+++ src/components/ExperienceBlock/ExperienceBlock.js
@@ -8,7 +8,7 @@ export const ExperienceBlock = ({title, position, date, description, contributio
         <div className='experience text-pad'>
             <div className='experience_header text-pad-paragraph'>
                 <div className='experience-left-block'>
-                     <img className='experience_image' src={process.env.PUBLIC_URL + '/' + icon} alt='experience_icon'/>
+                     <img className='experience_icon' src={process.env.PUBLIC_URL + '/' + icon} alt='experience_icon'/>
                     <div className='experience_info'>
                         <h3 className='experience_title'>{title}</h3>
                         <h3 className='experience_position'>{position}</h3>
